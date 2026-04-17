97859da058286add6f56a7dc9873e5acb2713773
prakash <ps8780909@gmail.com>
2026-04-18 01:24:34 +0530

    update db

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 const mongoURI = process.env.MONGO_URL;
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log("Connected to MongoDB successfully"))
-        .catch((error) => {
-            console.error("Error connecting to MongoDB:", error);
-            process.exit(1); // Exit the process if the database connection fails
-        });
-}
+const connectToMongo = () => {
+  mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
+    });
+};
 
 module.exports = connectToMongo;

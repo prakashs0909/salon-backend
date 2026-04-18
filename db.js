708d0c8b1d9e648f154a1b328e43c1b1a9725c6b
prakash <ps8780909@gmail.com>
2026-04-18 13:55:52 +0530

    update db

--- db.js
+++ db.js
@@ -1,10 +1,26 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 const mongoURI = process.env.MONGO_URL;
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI)
-    console.log("connected to mongo successfully");
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+const connectToMongo = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+    cached.promise = mongoose.connect(mongoURI, opts).then((mongoose) => {
+      console.log("connected to mongo successfully");
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.promise;
+};
 
 module.exports = connectToMongo;
